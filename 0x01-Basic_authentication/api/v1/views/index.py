9ead6bedb3648fc90221e63ec938dc6dd9ff3345
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-05-29 21:10:17 +0100

    [feat]: error handler 403

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
         - aborts with 401 error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+        - aborts with 403 error
+    """
+    abort(403)
