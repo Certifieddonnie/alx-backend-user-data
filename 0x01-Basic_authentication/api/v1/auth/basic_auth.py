209e2403847d37bfb806a1f7aa6ac5e0fb3ef946
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-05-30 22:47:15 +0100

    [feat]: User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,7 +7,8 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ Basic Auth Class """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """ returns Base64 part of the Authorization header
         """
         if authorization_header is None:
@@ -21,7 +22,8 @@ class BasicAuth(Auth):
         return basic[-1]
 
 
-    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
         """ returns the decoded value of a base64 string
         """
         if base64_authorization_header is None:
@@ -35,3 +37,22 @@ class BasicAuth(Auth):
             return decoded_value.decode('utf-8')
         except Exception:
             return None
+
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """ returns the user email and password from
+        the encoded string
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        x = re.search(':', decoded_base64_authorization_header)
+        if not x:
+            return (None, None)
+        else:
+            lst = re.split(':', decoded_base64_authorization_header)
+            user = lst[0]
+            email = lst[1]
+            return (user, email)
